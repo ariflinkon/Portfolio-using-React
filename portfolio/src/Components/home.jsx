3c8a1823017891c9f26565063001465a4f8bfa9b
Arif Jahan Linkon <arifjahanlinkon7931@gmail.com>
2025-05-28 22:31:55 +0600

    Updated UI design and left section

--- portfolio/src/Components/home.jsx
+++ portfolio/src/Components/home.jsx
@@ -1,24 +1,46 @@
-
 export default function HomePage() {
   return (
-    <div className="flex flex-col md:flex-row items-center justify-between mx-auto px-[150px] py-[150px]">
+    <div className="flex flex-col md:flex-row items-center justify-between max-w-screen-xl mx-auto px-6 md:px-12 lg:px-24 py-16 md:py-24">
       {/* Left Section */}
-      <div className="md:w-1/2 mb-10 md:mb-0">
-        <h1 className="text-4xl font-bold text-gray-900 mb-2">Hello, I’mBrooklyn Gilbert</h1>
-        <h2 className="text-xl text-gray-600 mb-4">I'm a Freelance UI/UX Designer and Developer based in London, England. I strives to build immersive and beautiful web applications through carefully crafted code and user-centric design.</h2>
-        <button className="bg-purple-600 text-white px-5 py-2 rounded-lg mb-6 hover:bg-blue-700 transition">
-         Say Hello!
+      <div className="w-full md:w-1/2 mb-10 md:mb-0 text-center md:text-left">
+        <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold text-gray-900 mb-4">
+          Hello, I’m Brooklyn Gilbert
+        </h1>
+        <h2 className="text-base sm:text-lg lg:text-xl text-gray-600 mb-6">
+          I'm a Freelance UI/UX Designer and Developer based in London, England.
+          I strive to build immersive and beautiful web applications through
+          carefully crafted code and user-centric design.
+        </h2>
+        <button className="bg-purple-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition">
+          Say Hello!
         </button>
+
+        {/* Subheadings */}
+        <div className="grid grid-cols-1 sm:grid-cols-3 gap-1 mt-10">
+         <div className="bg-[#EDD8FF] p-6 rounded-md text-center">
+          <h1 className="text-3xl font-bold text-gray-900">15Y.</h1>
+          <p className="text-gray-600">Experience</p>
+        </div>
+        <div className="bg-[#EDD8FF] p-6 rounded-md text-center">
+         <h1 className="text-3xl font-bold text-gray-900">250+</h1>
+         <p className="text-gray-600">Projects Done</p>
+        </div>
+         <div className="bg-[#EDD8FF] p-6 rounded-md text-center">
+         <h1 className="text-3xl font-bold text-gray-900">58</h1>
+         <p className="text-gray-600">Happy Clients</p>
+         </div>
+       </div>
+
       </div>
 
       {/* Right Section */}
-      <div className="flex justify-center">
+      <div className="w-full md:w-1/2 flex justify-center">
         <img
           src="/p-image.png"
           alt="Profile"
-          className="w-[536px] h-[636px] object-cover "
+          className="w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl h-auto object-cover"
         />
       </div>
     </div>
-  )
+  );
 }
